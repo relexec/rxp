3918425f8dd47ddd719262cda51708f4093caae3
Jay Pipes <jaypipes@gmail.com>
2026-05-13 16:24:11 -0400

    standardize object invalid errors
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

--- errors/object.go
+++ errors/object.go
@@ -1,31 +1,11 @@
 package errors
 
-import (
-	"fmt"
-)
-
 var (
-	ErrInvalidObject            = New("invalid object", WithCode(ErrCodeBadRequest))
-	ErrNilObject                = New("nil object parameter", WithWrap(ErrInvalidObject))
-	ErrObjectMissingKindVersion = New("object missing kindversion", WithWrap(ErrInvalidObject))
-	ErrObjectDomainRequired     = New("domain required", WithWrap(ErrInvalidObject))
-	ErrObjectNamespaceRequired  = New("namespace required", WithWrap(ErrInvalidObject))
+	ErrInvalidObject             = New("invalid object", WithCode(ErrCodeBadRequest))
+	ErrNilObject                 = New("nil object parameter", WithWrap(ErrInvalidObject))
+	ErrObjectKindVersionRequired = New("kindversion required", WithWrap(ErrInvalidObject))
+	ErrObjectNameRequired        = New("name required", WithWrap(ErrInvalidObject))
+	ErrObjectUUIDRequired        = New("uuid required", WithWrap(ErrInvalidObject))
+	ErrObjectDomainRequired      = New("domain required", WithWrap(ErrInvalidObject))
+	ErrObjectNamespaceRequired   = New("namespace required", WithWrap(ErrInvalidObject))
 )
-
-// ObjectMissingUUID returns a wrapped ErrObjectInvalid indicating the
-// supplied Object type is missing an identifier.
-func ObjectMissingUUID(typ any) error {
-	return New(
-		fmt.Sprintf("missing uuid for %q", typ),
-		WithWrap(ErrInvalidObject),
-	)
-}
-
-// ObjectMissingName returns a wrapped ErrObjectInvalid indicating the supplied
-// Object is missing a name.
-func ObjectMissingName(typ any, uuid any) error {
-	return New(
-		fmt.Sprintf("missing name for %q (%s)", typ, uuid),
-		WithWrap(ErrInvalidObject),
-	)
-}
